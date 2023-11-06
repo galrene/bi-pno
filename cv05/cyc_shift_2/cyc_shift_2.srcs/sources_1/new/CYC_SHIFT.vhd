947ddf13fb933cec29e0363176aa69a3d8b7a149
René Gál <galrene@fit.cvut.cz>
2023-11-07 00:46:50 +0100

    hw01: top-level fix stupid syntax errors

--- cv05/cyc_shift_2/cyc_shift_2.srcs/sources_1/new/CYC_SHIFT.vhd
+++ cv05/cyc_shift_2/cyc_shift_2.srcs/sources_1/new/CYC_SHIFT.vhd
@@ -9,24 +9,25 @@ entity CYC_SHIFT is
         OUTPUT   : out STD_LOGIC_VECTOR ( 7 downto 0 );
         COPY_NUM : out STD_LOGIC_VECTOR ( 7 downto 0 );
         COPY_AM  : out STD_LOGIC_VECTOR ( 7 downto 0 );
-        CLK      : in  STD_LOGIC;
+        CLK      : in  STD_LOGIC
     ); 
 end CYC_SHIFT;
 
 architecture CYC_SHIFT_BODY of CYC_SHIFT is
     component DATAPATH is
         port (
-            INPUT     : in STD_LOGIC_VECTOR ( 7 downto 0 );
-            LOAD_NUM  : in STD_LOGIC;
-            SH_LEFT   : in STD_LOGIC;
-            LOAD_AM   : in STD_LOGIC;
-            SIG_SHIFT : in STD_LOGIC;
+            INPUT     : in  STD_LOGIC_VECTOR ( 7 downto 0 );
+            LOAD_NUM  : in  STD_LOGIC;
+            SH_LEFT   : in  STD_LOGIC;
+            LOAD_AM   : in  STD_LOGIC;
+            SIG_SHIFT : in  STD_LOGIC;
             OUTPUT    : out STD_LOGIC_VECTOR ( 7 downto 0 );
-            CLK       : in STD_LOGIC
-            -- COPY_NUM : out STD_LOGIC_VECTOR ( 7 downto 0 );
+            CLK       : in  STD_LOGIC
+            -- COPY_IN : out STD_LOGIC_VECTOR ( 7 downto 0 );
             -- COPY_AM : out STD_LOGIC_VECTOR ( 2 downto 0 );
         );
     end component DATAPATH;
+
     
     component CONTROLLER is
         port (
@@ -36,45 +37,23 @@ architecture CYC_SHIFT_BODY of CYC_SHIFT is
     end component CONTROLLER;
 begin
     DATA_INST : DATAPATH port map (
-        INPUT     => INPUT;
-        LOAD_NUM  => LOAD_NUM;
-        SH_LEFT   => SHIFT_LEFT;
-        LOAD_AM   => LOAD_AM;
-        SIG_SHIFT => SIG_SHIFT;
-        OUTPUT    => OUTPUT;
-        CLK       => CLK;
+        INPUT     => INPUT,
+        LOAD_NUM  => LOAD_NUM,
+        SH_LEFT   => SHIFT_LEFT,
+        LOAD_AM   => LOAD_AM,
+        SIG_SHIFT => SIG_SHIFT,
+        OUTPUT    => OUTPUT,
+        CLK       => CLK
     );
 
     CNTR_INST : CONTROLLER port map (
-        BUT_0     => BUT_0;
-        BUT_1     => BUT_1;
-        BUT_2     => BUT_2;
-        CLK       => CLK;
-        LOAD_NUM  => LOAD_NUM;
-        LOAD_AM   => LOAD_AM;
-        SH_LEFT   => SH_LEFT;
-        SIG_SHIFT => SIG_SHIFT;
+        BUT_0     => BUT_0,
+        BUT_1     => BUT_1,
+        BUT_2     => BUT_2,
+        CLK       => CLK,
+        LOAD_NUM  => LOAD_NUM,
+        LOAD_AM   => LOAD_AM,
+        SH_LEFT   => SH_LEFT,
+        SIG_SHIFT => SIG_SHIFT
     );
 end architecture CYC_SHIFT_BODY;
-
----------------------------------------------
-
-architecture SOFTWARE_MODEL of CYC_SHIFT is
-begin
-
-    SW_MODEL : process
-        variable NUM, AM;
-    begin
-        wait until BUT_0 = '1';
-        NUM := UNSIGNED(INPUT);
-        COPY_NUM <= INPUT;
-        wait until BUT_1 = '1' or BUT_2 = '1';
-        if BUT_1 = '1' then
-
-            OUTPUT <= ( NUM sll AM );
-        else
-            OUTPUT <= ( NUM << AM );
-        end if;
-    end process;
-
-end architecture SOFTWARE_MODEL;
