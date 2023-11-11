74207879f762dbc1ec3db7cf190bf922d4a2f184
René Gál <galrene@fit.cvut.cz>
2023-11-11 09:18:58 +0100

    hw01: refactor datapath

--- cv05/cyc_shift_2/cyc_shift_2.srcs/sources_1/new/datapath.vhd
+++ cv05/cyc_shift_2/cyc_shift_2.srcs/sources_1/new/datapath.vhd
@@ -9,27 +9,32 @@ entity DATAPATH is
         LOAD_AM   : in  STD_LOGIC;
         SIG_SHIFT : in  STD_LOGIC;
         OUTPUT    : out STD_LOGIC_VECTOR ( 7 downto 0 );
-        CLK       : in  STD_LOGIC
-        -- COPY_IN : out STD_LOGIC_VECTOR ( 7 downto 0 );
-        -- COPY_AM : out STD_LOGIC_VECTOR ( 2 downto 0 );
+        CLK       : in  STD_LOGIC;
+        ------------------------------------------------
+        COPY_NUM   : out STD_LOGIC_VECTOR ( 7 downto 0 );
+        COPY_AM    : out STD_LOGIC_VECTOR ( 2 downto 0 );
+        COPY_DIR   : out STD_LOGIC -- direction of the shift, not really a copy since an "original" doesn't exist (handled by an internal signal)
+                                   -- '0' == right shift, '1' == left shift
     );
 end entity DATAPATH;
 
 architecture DATAPATH_BODY of DATAPATH is
 
-    signal S_NUM       : STD_LOGIC_VECTOR ( 7 downto 0 );
-    signal S_AM        : STD_LOGIC_VECTOR ( 2 downto 0 );
-    signal S_AM_NO_DIR : STD_LOGIC_VECTOR ( 2 downto 0 );
-    signal S_RES       : STD_LOGIC_VECTOR ( 7 downto 0 );
+    signal NUM                 : STD_LOGIC_VECTOR ( 7 downto 0 );
+    signal SH_AMOUNT           : STD_LOGIC_VECTOR ( 2 downto 0 );
+    signal SH_AMOUNT_NO_DIR    : STD_LOGIC_VECTOR ( 2 downto 0 ); -- raw SH_AMOUNT before deciding which direction to shift in  
+    signal RES                 : STD_LOGIC_VECTOR ( 7 downto 0 );
 
 begin
 
-
+    -- register that loads the number for shifting
     NUM_REG : process ( CLK )
     begin
         if CLK = '1' and CLK'EVENT then
             if LOAD_NUM = '1' then
-                S_NUM <= INPUT;
+                NUM <= INPUT;
+                ---
+                COPY_NUM <= INPUT;
             end if;
         end if;
     end process NUM_REG;
@@ -39,19 +44,23 @@ begin
     begin
         if CLK = '1' and CLK'EVENT then
             if LOAD_AM = '1' then
-                S_AM_NO_DIR <= INPUT ( 2 downto 0 );
+                SH_AMOUNT_NO_DIR <= INPUT ( 2 downto 0 );
+                ---
+                COPY_AM <= INPUT ( 2 downto 0 );
             end if;
         end if;
     end process SH_AM_REG;
     
     -- process deciding shift direction
-    SH_DIR : process ( S_AM_NO_DIR )
+    SH_DIR : process ( SH_AMOUNT_NO_DIR )
     begin
-        S_AM <= S_AM_NO_DIR;
+        SH_AMOUNT <= SH_AMOUNT_NO_DIR;
+        COPY_DIR <= '0';
         if SH_LEFT = '1' then
             -- converting to two's complement converts a right shift to a left shift
-            S_AM <= STD_LOGIC_VECTOR ( not ( UNSIGNED ( S_AM_NO_DIR ) )
-                                       + 1 );
+            SH_AMOUNT <= STD_LOGIC_VECTOR ( UNSIGNED ( not SH_AMOUNT_NO_DIR ) +  1 );
+            ---
+            COPY_DIR <= '1'; 
         end if;
     end process SH_DIR;
 
@@ -60,7 +69,7 @@ begin
     begin
         if CLK = '1' and CLK'EVENT then
             if SIG_SHIFT = '1' then
-                OUTPUT <= S_RES;
+                OUTPUT <= RES;
             end if;
         end if;
     end process RES_REG;
