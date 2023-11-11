9d8ea483efde64b4185d43ca25b0de367a158f9c
René Gál <galrene@fit.cvut.cz>
2023-11-11 09:42:07 +0100

    hw01: top level entity for implementation

---
+++ cv05/cyc_shift_2/cyc_shift_2.srcs/sources_1/new/shift_and_io.vhd
@@ -0,0 +1,78 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+entity SHIFT_AND_IO is
+   port (
+      SWITCH     : in  STD_LOGIC_VECTOR (7 downto 0);
+      BUT_0       : in  STD_LOGIC;
+      BUT_1       : in  STD_LOGIC;
+      BUT_2       : in  STD_LOGIC;
+      BUT_3       : in  STD_LOGIC;
+      SEGMENT    : out STD_LOGIC_VECTOR (6 downto 0);    -- 7 segments of the display
+      DP         : out STD_LOGIC;                        -- floating point
+      DIGIT      : out STD_LOGIC_VECTOR (3 downto 0);    -- 4 digits of the display
+      CLK        : in  STD_LOGIC
+   );
+end entity SHIFT_AND_IO;
+
+architecture SHIFT_AND_IO_BODY of SHIFT_AND_IO is
+    component CYC_SHIFT is
+        port (
+            INPUT    : in  STD_LOGIC_VECTOR ( 7 downto 0 );
+            BUT_0    : in  STD_LOGIC;
+            BUT_1    : in  STD_LOGIC;
+            BUT_2    : in  STD_LOGIC;
+            OUTPUT   : out STD_LOGIC_VECTOR ( 7 downto 0 );
+            CLK      : in  STD_LOGIC;
+            -----------------------------------------------
+            COPY_NUM : out STD_LOGIC_VECTOR ( 7 downto 0 );
+            COPY_AM  : out STD_LOGIC_VECTOR ( 7 downto 0 );
+            COPY_DIR : out STD_LOGIC 
+        ); 
+    end component CYC_SHIFT;
+    
+   component HEX2SEG is
+      port (
+         DATA     : in  STD_LOGIC_VECTOR (11 downto 0);   -- input data for display ( NUM(8), SHIFT_DIR(1), SHIFT_AM(3) ) 
+         CLK      : in  STD_LOGIC;
+         SEGMENT  : out STD_LOGIC_VECTOR (6 downto 0);    -- 7 segments of the display
+         DP       : out STD_LOGIC;                        -- floating point
+         DIGIT    : out STD_LOGIC_VECTOR (3 downto 0)     -- 4 digits of the display
+      );
+   end component HEX2SEG;
+   -- signals --
+   signal OUTPUT, DATA      : STD_LOGIC_VECTOR (15 downto 0);  -- result, display data
+   signal COPY_NUM, COPY_AM : STD_LOGIC_VECTOR (7 downto 0);   -- input operands copy 
+   signal COPY_DIR          : STD_LOGIC;                       -- direction indicator copy
+begin
+   SHIFT_INST : CYC_SHIFT port map (
+                  INPUT    => SWITCH,
+                  BUT_0    => BUT_0,
+                  BUT_1    => BUT_1,
+                  BUT_2    => BUT_2,
+                  OUTPUT   => OUTPUT,
+                  COPY_NUM => COPY_NUM,
+                  COPY_AM  => COPY_AM,
+                  COPY_DIR => COPY_DIR,
+                  CLK      => CLK
+                );
+   
+   DISP_INST : HEX2SEG port map (
+                  DATA     => DATA,
+                  CLK      => CLK,
+                  SEGMENT  => SEGMENT,
+                  DP       => DP,
+                  DIGIT    => DIGIT
+               );
+    
+   -- BTN3 switches between output or original input operands (number shift, shift direction, shift amount)
+   DISPLAY_MUX : process (BTN3, OUTPUT, COPY_NUM, COPY_AM, COPY_DIR)
+   begin
+      if BTN3 = '0' then
+         DATA <= OUTPUT;
+      else
+         DATA <= (COPY_NUM & COPY_DIR & COPY_AM);
+      end if; 
+   end process DISPLAY_MUX;
+
+
+end architecture SHIFT_AND_IO_BODY;
