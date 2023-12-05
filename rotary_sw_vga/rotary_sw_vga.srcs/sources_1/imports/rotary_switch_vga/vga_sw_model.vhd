7e07e7bf9ab81cbfb1e944e95085b1fa2a5dfe30
René Gál <galrene@fit.cvut.cz>
2023-12-05 02:25:56 +0100

    hw02: software model - much much simplified

--- rotary_sw_vga/rotary_sw_vga.srcs/sources_1/imports/rotary_switch_vga/vga_sw_model.vhd
+++ rotary_sw_vga/rotary_sw_vga.srcs/sources_1/imports/rotary_switch_vga/vga_sw_model.vhd
@@ -1,4 +1,7 @@
-entitiy VGA is
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+entity VGA is
     port (
        REGX      : in  std_logic_vector (7 downto 0); -- X
        REGY      : in  std_logic_vector (7 downto 0); -- Y
@@ -10,101 +13,76 @@ entitiy VGA is
        CLK       : in  std_logic;                     -- 100 MHz
        RESET     : in  std_logic
     );
-end entitiy VGA;
+end entity VGA;
 
 architecture SOFTWARE_MODEL of VGA is
-    variable VGA_X                     : INTEGER := 0;
-    variable VGA_Y                     := 0;
-
-    signal   CLK_DIVIDER               : INTEGER := 0;
-    signal   VGA_CLK                   : std_logic;
-    constant PIXEL_PERIOD              = 1 / 25e6; -- in seconds
     
-    -- in pixels
-    constant VISIBLE_SCREEN_WIDTH      = 640;
-    constant H_BACK_PORCH              = 16;
-    constant H_FRONT_PORCH             = 16;
-    constant HSYNC_PULSE_WIDTH         = 128;
+    -- quantities in pixels
+    constant VISIBLE_SCREEN_WIDTH      : NATURAL := 640;
+    constant H_BACK_PORCH              : NATURAL := 16;
+    constant H_FRONT_PORCH             : NATURAL := 16;
+    constant HSYNC_PULSE_WIDTH         : NATURAL := 128;
 
-    constant VISIBLE_SCREEN_HEIGHT     = 480;
-    constant V_BACK_PORCH              = 29;
-    constant V_FRONT_PORCH             = 10;
-    constant VSYNC_PULSE_WIDTH         = 2;
+    constant VISIBLE_SCREEN_HEIGHT     : NATURAL := 480;
+    constant V_BACK_PORCH              : NATURAL := 29;
+    constant V_FRONT_PORCH             : NATURAL := 10;
+    constant VSYNC_PULSE_WIDTH         : NATURAL := 2;
 begin
---NOTE: H_SYNC and V_SYNC are active on 0
-    
-    -- quarter the period of the main clock
-    VGA_CLK : process (CLK)
-    begin 
-        if CLK = '1' and CLK'event then  
-            if RESET = '1' then
-                CLK_DIVIER <= 0;
-                VGA_CLK <= '0';    
-            else
-                CLK_DIVIDER <= CLK_DIVIDER + 1;
-            end if;
-            if CLK_DIVIDER = 2 then
-                CLK_DIVIDER <= 0;
-                VGA_CLK <= not VGA_CLK;
-            end if;
-        end if;
-    end process;
-
-
-    INTERAL_COUNTERS : process (VGA_CLK)
+    --NOTE: H_SYNC and V_SYNC are active on 0
+    PIXEL_GEN : process
+        variable VGA_X                     : NATURAL := 0;
+        variable VGA_Y                     : NATURAL := 0;
     begin
-       if VGA_CLK = '1' and VGA_CLK'event then
-            if RESET = '1' then
-                VGA_X     :=  0;
-                VGA_Y     :=  0;
-                VGA_HSYNC := '1';
-                VGA_VSYNC := '1';
-            elsif VGA_X = H_BACK_PORCH + VISIBLE_SCREEN_WIDTH + H_FRONT_PORCH + HSYNC_PULSE_WIDTH then
-                VGA_X := 0;
-                if VGA_Y = V_BACK_PORCH + VISIBLE_SCREEN_HEIGHT + V_FRONT_PORCH + VSYNC_PULSE_WIDTH then
-                    VGA_Y := 0;
-                else
-                    VGA_Y := VGA_Y + 1;
-                end if;
+
+        -- every 4 clock cycles, advance the local counters - simulates 25MHz clock
+        -- vga_clk = '0'
+        for i in 0 to 2 loop
+            wait until CLK = '1' and CLK'event;
+        end loop;
+        
+        VGA_RED   <= '0';
+        VGA_GREEN <= '0';
+        VGA_BLUE  <= '0';
+        VGA_HSYNC <= '1';
+        VGA_VSYNC <= '1';
+
+        if VGA_X = UNSIGNED(REGX) + H_BACK_PORCH + HSYNC_PULSE_WIDTH then
+            -- when at the given X coord, generate a white pixel
+            VGA_RED   <= '1';
+            VGA_GREEN <= '1';
+            VGA_BLUE  <= '1';
+        end if;
+        if VGA_Y = UNSIGNED(REGY) + V_BACK_PORCH + VSYNC_PULSE_WIDTH then
+            -- when at the given Y coordinate, generate a whole white line
+            VGA_RED   <= '1';
+            VGA_GREEN <= '1';
+            VGA_BLUE  <= '1';
+        end if;
+        -- when at end of line, start new line
+        if VGA_X >= H_BACK_PORCH + VISIBLE_SCREEN_WIDTH + H_FRONT_PORCH then
+            VGA_HSYNC <= '0';
+        end if;    
+            -- when at end of frame, start new frame
+        if VGA_Y >= V_BACK_PORCH + VISIBLE_SCREEN_HEIGHT + V_FRONT_PORCH then
+            VGA_VSYNC <= '0';
+        end if;
+        
+        -- increment local counters
+        if VGA_X = H_BACK_PORCH + VISIBLE_SCREEN_WIDTH + H_FRONT_PORCH + HSYNC_PULSE_WIDTH then
+            VGA_X := 0;
+            if VGA_Y = V_BACK_PORCH + VISIBLE_SCREEN_HEIGHT + V_FRONT_PORCH + VSYNC_PULSE_WIDTH then
+                VGA_Y := 0;
             else
-                VGA_X := VGA_X + 1;
+                VGA_Y := VGA_Y + 1;
             end if;
-       end if;
-    end process;
+        else
+            VGA_X := VGA_X + 1;
+        end if;
+        
+        --vga_clk = '1'
+        for i in 0 to 2 loop
+            wait until CLK = '1' and CLK'event;
+        end loop;
 
-    
-
-    PIXEL_GEN : process (VGA_CLK)
-    if VGA_Y = REGY + V_BACK_PORCH + VSYNC_PULSE_WIDTH then
-    -- when at the given Y coordinate, generate a whole white line
-       VGA_RED   = '1';
-       VGA_GREEN = '1';
-       VGA_BLUE  = '1';
-       wait for VISIBLE_SCREEN_WIDTH * PIXEL_PERIOD;
-       VGA_RED   = '0';
-       VGA_GREEN = '0';
-       VGA_BLUE  = '0';
-    elsif VGA_X = REGX + H_BACK_PORCH + HSYNC_PULSE_WIDTH then
-    -- when at the given X coord, generate a white pixel
-       VGA_RED   = '1';
-       VGA_GREEN = '1';
-       VGA_BLUE  = '1';
-       wait for PIXEL_PERIOD;
-       VGA_RED   = '0';
-       VGA_GREEN = '0';
-       VGA_BLUE  = '0';
-    end if;
-    -- when at end of line, start new line
-    if VGA_X = H_BACK_PORCH + VISIBLE_SCREEN_WIDTH + H_FRONT_PORCH then
-       VGA_HSYNC = '0';
-       wait for HSYNC_PULSE_WIDTH * PIXEL_PERIOD;
-       VGA_HSYNC = '1';
-    end if;    
-    -- when at end of frame, start new frame
-    if VGA_Y = V_BACK_PORCH + VISIBLE_SCREEN_HEIGHT + V_FRONT_PORCH then
-       VGA_VSYNC = '0';
-       wait for VSYNC_PULSE_WIDTH * PIXEL_PERIOD;
-       VGA_VSYNC = '1';
-    end if;
     end process PIXEL_GEN;
 end architecture SOFTWARE_MODEL;
