19f8425fdf14ff36789aaba4f6f41bed67cc63e3
René Gál <galrene@fit.cvut.cz>
2023-12-18 02:01:24 +0100

    hw02: placeholder vhd files

---
+++ rotary_dec/rotary_dec.srcs/sources_1/new/controller.vhd
@@ -0,0 +1,22 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity CONTROLLER is
+--  Port ( );
+end entity CONTROLLER;
+
+architecture CONTROLLER_BODY of CONTROLLER is
+
+begin
+
+
+end architecture CONTROLLER_BODY;
---
+++ rotary_dec/rotary_dec.srcs/sources_1/new/rotary_dec.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 12/18/2023 01:24:00 AM
+-- Design Name: 
+-- Module Name: ROTARY_DEC - ROTARY_DEC_BODY
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ROTARY_DEC is
+--  Port ( );
+end ROTARY_DEC;
+
+architecture ROTARY_DEC_BODY of ROTARY_DEC is
+
+begin
+
+
+end ROTARY_DEC_BODY;
