7439083e02a6301f4275f51868de9586b6a7686e
René Gál <galrene@fit.cvut.cz>
2023-12-18 17:42:20 +0100

    hw02: datapath look good

--- rotary_dec/rotary_dec.srcs/sources_1/new/datapath.vhd
+++ rotary_dec/rotary_dec.srcs/sources_1/new/datapath.vhd
@@ -1,43 +1,75 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 12/18/2023 01:22:15 AM
--- Design Name: 
--- Module Name: DATAPATH - DATAPATH_BODY
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity DATAPATH is
---  Port ( );
+    port (
+        ROTARY    : in  STD_LOGIC_VECTOR ( 2 downto 0 );
+        X         : in  STD_LOGIC; -- x := select reg x, not x := select reg y
+        REGX      : out STD_LOGIC_VECTOR ( 7 downto 0 );
+        REGY      : out STD_LOGIC_VECTOR ( 7 downto 0 );
+        CLK       : in  STD_LOGIC;
+        RST_DP    : in  STD_LOGIC;
+    );
 end DATAPATH;
 
 architecture DATAPATH_BODY of DATAPATH is
+    component QUADRATURE_DEC is
+        port (
+            A, B, CLK, RESET : in STD_LOGIC;
+            INC, EN          : out STD_LOGIC -- increment/decrement, enable counting
+        );
+    end component QUADRATURE_DEC;
 
+    signal REGX_SIG        : STD_LOGIC_VECTOR ( 7 downto 0 );
+    signal REGY_SIG        : STD_LOGIC_VECTOR ( 7 downto 0 );
+    signal INCREMENT       : STD_LOGIC;
+    signal ENABLE_COUNTING : STD_LOGIC;
 begin
 
+    REGX <= REGX_SIG;
+    REGY <= REGY_SIG;
 
-end DATAPATH_BODY;
+    POS_X_REG : process ( CLK )
+    begin
+        if CLK = '1' and CLK'EVENT then
+            if RST_DP = '1' then
+                REG_X_SIG <= '0';
+            elsif ENABLE_COUNTING = '1' and X = '1' then
+            -- at posedge, when X register is selected and counting is enabled
+                if INCREMENT = '1' then
+                    REG_X_SIG =+ 1;
+                else
+                    REG_X_SIG -= 1;
+                end if;
+            end if;
+        end if;
+    end process POS_X_REG;
+
+    POS_Y_REG : process ( CLK )
+    begin
+        if CLK = '1' and CLK'EVENT then
+            if RST_DP = '1' then
+                REG_Y_SIG <= '0';
+            elsif ENABLE_COUNTING = '1' and X = '0' then
+            -- at posedge, when X register is selected and counting is enabled
+                if INCREMENT = '1' then
+                    REG_Y_SIG =+ 1;
+                else
+                    REG_Y_SIG -= 1;
+                end if;
+            end if;
+        end if;
+    end process POS_Y_REG;
+    
+    -------------------------------------
+
+    QUAD_DEC : QUADRATURE_DEC port map (
+        A     => ROTARY(0),
+        B     => ROTARY(1),
+        CLK   => CLK,
+        RESET => RST_DP,
+        INC   => INCREMENT,
+        EN    => ENABLE_COUNTING
+    )
+
+end architecture DATAPATH_BODY;
